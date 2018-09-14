2b0acbd420c19c0a9f8a6a677e5841a85aef27a6
guoxiaodi <guody001@gmail.com>
2018-09-14 10:53:57 +0800

    响应报文格式化处理、api错误处理

--- middlewares/auth_token.js
+++ middlewares/auth_token.js
@@ -1,29 +1,28 @@
 const authUtil = require('../utils/authUtil');
-const Result = require('../entity/Result');
+const ApiErrorNames = require('../config/ApiErrorNames');
+const APIError = require('./result').APIError;
 
 /**
  * access_token验证中间件
  */
 let authToken = async (ctx,next) => {
-    await next();
     if(ctx.method == 'OPTIONS'){
         return;
     }
-    
     // 用户登录时，不用验证
-    if(ctx.url == '/api/user/login'){
+    if(ctx.url == '/api/user/login' || ctx.url == '/api/user/regist'){
         return
     }
-    let token; 
-    token = ctx.headers.token;   
+
+    await next();
+    let token = ctx.headers.token;  
     if (!token) {
-        let res = new Result('403','token为空，请重新登录'); 
-        ctx.body = res;
-        console.log(111)
+        throw new APIError(ApiErrorNames.TOKEN_NOT_FOUND);
         return;
-    } 
+    }else{
+        authUtil.verifyToken(token);
+    }
     
- 
 }
 
 module.exports = authToken;
